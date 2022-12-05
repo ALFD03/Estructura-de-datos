9b6fbc2685b781c1c43a53dd00f0b889aaa1ae6a
Andy0Gx <Andy20ogx@gmail.com>
2022-12-04 21:36:27 -0400

    Add Option | Done

--- Biblioteca_de_juegos.cpp
+++ Biblioteca_de_juegos.cpp
@@ -2,29 +2,65 @@
 // Biblioteca de juego con listas
 
 #include <iostream> //Librerias
-#include <conio.h>
+#include <string>
 using namespace std;
 
+struct Juego {
 
-struct BibliotecaDeJuegos {
-
-char NombreJuego [50];
-char FechaLanzamiento [10];
+string NombreJuego ;
+string FechaLanzamiento ;
 int Valoracion;
-char Plataformas [50];
-char Genero [50];
-char Desarrollador [50];
-int Precio;
+string Plataformas ;
+string Genero ;
+string Desarrollador ;
 
-}
+Juego *siguiente;
+};
+
+void addJuego (Juego *&, string, string, int, string, string, string);
 
 
 int main() {
-	
+	Juego *BibliotecaDeJuegos = NULL;
+	int Valoracion;
+	string NombreJuego , FechaLanzamiento , Plataformas , Genero , Desarrollador ;
 
+	cout << "Introduzca el nombre del juego: ";
+	getline(cin, NombreJuego);
+	cout << "Introduzca la fecha de lanzamiento: ";
+	getline(cin, FechaLanzamiento);
+	cout << "Introduzca la valoracion: ";
+	cin >> Valoracion;
+	cin.ignore();
+	cout << "Introduzca las plataformas: ";
+	getline(cin, Plataformas);
+	cout << "Introduzca el genero: ";
+	getline(cin, Genero);
+	cout << "Introduzca el desarrollador: ";
+	getline(cin, Desarrollador);
+
+	addJuego(BibliotecaDeJuegos, NombreJuego, FechaLanzamiento, Valoracion, Plataformas, Genero, Desarrollador);
 	
-	getch();
 	return 0;
+}
+
+void addJuego (Juego *& BibliotecaDeJuegos, string NombreJuego, string FechaLanzamiento, int Valoracion, string Plataformas, string Genero, string Desarrollador) {
+	Juego *nuevoJuego = new Juego();
+	nuevoJuego -> NombreJuego = NombreJuego;
+	nuevoJuego -> FechaLanzamiento = FechaLanzamiento;
+	nuevoJuego -> Valoracion = Valoracion;
+	nuevoJuego -> Plataformas = Plataformas;
+	nuevoJuego -> Genero = Genero;
+	nuevoJuego -> Desarrollador = Desarrollador;
+
+	Juego *aux1 = BibliotecaDeJuegos;
+	Juego *aux2;
+
+	nuevoJuego ->siguiente = aux1;
+
+	cout << "El juego se agrego correctamente a la biblioteca de juegos" << endl;
+
+
 }
 
 
